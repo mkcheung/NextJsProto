5c38d9f77ea1545e27d82f07585dfbbca76def80
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-03-12 14:30:26 -0700

    add and demonstrate use of Image tag

--- components/main-header.js
+++ components/main-header.js
@@ -1,11 +1,12 @@
 import Link from "next/link"
+import Image from "next/image"
 import logoImg from '@/assets/logo.png'
 import classes from './main-header.module.css'
 
 export default function MainHeader() {
     return <header className={classes.header}>
         <Link className={classes.logo} href="/">
-            <img src={logoImg.src} alt="a plate with food on it" />
+            <Image src={logoImg} alt="a plate with food on it" priority />
             NextLevel Food
         </Link>
 
